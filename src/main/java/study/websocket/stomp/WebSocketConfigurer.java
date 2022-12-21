974a337f37ff20b80c65f6136517734184de617a
sungmingt <qotjdals8313@naver.com>
2022-12-22 00:28:21 +0900

    add websocket configurer (stomp)

---
+++ src/main/java/study/websocket/stomp/WebSocketConfigurer.java
@@ -0,0 +1,24 @@
+package study.websocket.stomp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker //STOMP 사용
+public class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") //웹소켓 서버 endpoint
+                .setAllowedOrigins("*");
+    }
+}
